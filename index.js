9630b31a98800e74b83da4f253a426a7fca6d42a
CLEITIN20 <77171065+CLEITIN20@users.noreply.github.com>
2021-01-17 12:50:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -188,7 +188,7 @@ async function starts() {
 				} catch {
 					ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 				}
-				teks = `Oiin @${num.split('@')[0]}\nBem vindor ao *${mdata.subject}*`
+				teks = `iae membro novo @${num.split('@')[0]}\nBem vindo ao grupo *${mdata.subject}*`
 				let buff = await getBuffer(ppimg)
 				client.sendMessage(mdata.id, buff, MessageType.image, {caption: teks, contextInfo: {"mentionedJid": [num]}})
 			} else if (anu.action == 'remove') {
